e27e7b66b6be62c6271f9309eba0ea6be68adfa5
IPE012023 <j.herold@ipe-evaluation.de>
2024-10-15 13:35:14 +0200

    more meta data

--- app/datenschutz/page.tsx
+++ app/datenschutz/page.tsx
@@ -1,4 +1,31 @@
 import Datenschutz from '@/components/Datenschutz'
+import { baseMetadata } from '@/utils/const-metadata';
+
+export const metadata = {
+  ...baseMetadata,
+  title: "Datenschutz - Pflegeversicherung Reform",
+  description: "Datenschutz und rechtliche Hinweise für die Pflegeversicherung Reform.",
+  openGraph: {
+    ...baseMetadata.openGraph,
+    title: "Datenschutz - Pflegeversicherung Reform",
+    description: "Alle rechtlichen Hinweise zur Seite Reform Pflegeversicherung.",
+    url: "https://kollaps-pflegeversicherung.vercel.app/datenschutz",
+    images: [
+      {
+        url: "https://kollaps-pflegeversicherung.vercel.app/werter.png",
+        width: 778,
+        height: 318,
+        alt: "Datenschutz Reform Pflegeversicherung",
+      },
+    ],
+  },
+  twitter: {
+    ...baseMetadata.twitter,
+    title: "Datenschutz - Pflegeversicherung Reform",
+    description: "Rechtliche Hinweise für die Pflegeversicherung Reform.",
+    images: ["https://kollaps-pflegeversicherung.vercel.app/werter.png"],
+  },
+};
 
 export default function DatenschutzPage() {
   return (
