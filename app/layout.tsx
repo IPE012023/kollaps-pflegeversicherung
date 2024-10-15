0dfda1c6d7f51e1095040ccc570e896c7745c5c2
IPE012023 <j.herold@ipe-evaluation.de>
2024-10-15 11:30:36 +0200

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,8 @@ import { baseMetadata } from "@/utils/const-metadata";
 export const metadata: Metadata = {
   ...baseMetadata, // Spread the base metadata
   title: "Kollaps Verhindern - Pflegeversicherung reformieren",
-  description: "Drohender Kollaps der Pflegeversicherung: Steigende Beiträge sind unausweichlich, um steigende Kosten zu decken und eine Überlastung des Systems verhindern.",
+  description:
+    "Drohender Kollaps der Pflegeversicherung: Steigende Beiträge sind unausweichlich, um steigende Kosten zu decken und eine Überlastung des Systems verhindern.",
 };
 
 export default function RootLayout({
@@ -18,6 +19,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="de">
+      <head>
+        <meta
+          property="og:title"
+          content="Kollaps Verhindern - Pflegeversicherung reformieren"
+        />
+        <meta
+          property="og:description"
+          content="Drohender Kollaps der Pflegeversicherung: Steigende Beiträge sind unausweichlich, um steigende Kosten zu decken und eine Überlastung des Systems verhindern."
+        />
+        <meta
+          property="og:image"
+          content="https://kollaps-pflegeversicherung.vercel.app/images/kollaps.jpg"
+        />
+        <meta
+          property="og:url"
+          content="https://kollaps-pflegeversicherung.vercel.app/"
+        />
+      </head>
       <body>
         <ThemeProvider
           attribute="class"
@@ -27,13 +46,11 @@ export default function RootLayout({
         >
           <div className="flex flex-col min-h-screen">
             <Header />
-            <main className="flex-grow">
-              {children}
-            </main>
+            <main className="flex-grow">{children}</main>
             <Footer />
           </div>
         </ThemeProvider>
       </body>
     </html>
   );
-;}
+}
